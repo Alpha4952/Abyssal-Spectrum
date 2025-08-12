0034dc4f470fe826667097711d21db4b5e692daa
Alpha4952 <longnguyen6907@gmail.com>
2025-08-12 19:00:51 +0700

    Initial commit

---
+++ src/main/java/alpheta/abyssal_spectrum/AbyssalSpectrumDataGenerator.java
@@ -0,0 +1,11 @@
+package alpheta.abyssal_spectrum;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class AbyssalSpectrumDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
